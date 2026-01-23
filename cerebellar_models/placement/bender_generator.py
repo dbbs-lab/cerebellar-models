fix: split is_target_wrong tests in two, for rotation and scaling
Ease constraints on BC: rotation check for axon now based on ratio distance from PL compared to soma's equivalent

cerebellar_models/placement/bender_generator.py
@@ -162,22 +162,36 @@ class GolgiGenerator(BenderGenerator, classmap_entry="golgi_bender"):
 class BasketGenerator(BenderGenerator, classmap_entry="basket_bender"):
     """Specific bender for basket cell."""
 
+    max_ratio_distance_axon_pl = None
+
     def is_target_wrong(self, source, new_target, branch_labels=None):
         if super().is_target_wrong(source, new_target, branch_labels):
             return True
 
         # dendrites should remain in molecular layer
-        current_abv = self.get_lay_abv(new_target)
-        if not has_label(branch_labels, "axon"):
-            return "mo" not in current_abv
+        return not has_label(branch_labels, "axon") and "mo" not in self.get_lay_abv(new_target)
+
+    def is_rotation_wrong(self, source, new_target, branch_labels=None):
+        if super().is_rotation_wrong(source, new_target, branch_labels):
+            return True
 
         # axon should get closer to Purkinje layer.
-        current_dist = self.voxel_data_of(new_target, self.thicknesses())
-        old_dist = self.voxel_data_of(source, self.thicknesses())
-        if "mo" in current_abv:
-            return current_dist[1] > 62.5 and old_dist[1] < current_dist[1]
-        elif "gr" in current_abv:
-            return 37.5 < current_dist[1] < old_dist[1]
+        elif has_label(branch_labels, "axon") and np.any(
+            self.partition.mask_source.voxel_of(new_target)
+            != self.partition.mask_source.voxel_of(source)
+        ):
+            ratio_mol = self.voxel_data_of(new_target, self.thicknesses())[1]
+            if "mol" not in self.get_lay_abv(new_target):
+                ratio_mol -= 25.0  # retrieve thickness of PL
+            ratio_mol /= self.local_layer_thickness(new_target)
+            return ratio_mol > self.max_ratio_distance_axon_pl
+
+    def deform_morphology(self, morphology):
+        origin = morphology.points[0]
+        self.max_ratio_distance_axon_pl = self.voxel_data_of(origin, self.thicknesses())[
+            1
+        ] / self.local_layer_thickness(origin)
+        return super().deform_morphology(morphology)
 
 
 @config.node

cerebellar_models/placement/morphology_bender.py
@@ -269,6 +269,12 @@ class MorphologyBender:
             self.partition.mask_source.voxel_of(new_target),
         )
 
+    def is_scaling_wrong(self, source, new_target, branch_labels=None):
+        return self.is_target_wrong(source, new_target, branch_labels)
+
+    def is_rotation_wrong(self, source, new_target, branch_labels=None):
+        return self.is_target_wrong(source, new_target, branch_labels)
+
     def delete_point(self, branch, i):
         """
         Delete a point in a morphology branch and translate the subsequent points and children
@@ -300,7 +306,7 @@ class MorphologyBender:
         new_voxel = self.partition.mask_source.voxel_of(new_target)
         return (
             np.all(new_voxel == last_voxel)
-            or self.is_target_wrong(source, new_target, branch_labels),
+            or self.is_rotation_wrong(source, new_target, branch_labels),
             new_voxel,
         )
 
@@ -368,7 +374,7 @@ class MorphologyBender:
         if (
             np.linalg.norm(old_rots.rotation_to_correct.as_euler("xyz")) > 1e-5
             and inc == 1.0
-            and not self.is_target_wrong(
+            and not self.is_rotation_wrong(
                 source,
                 (scaled_diff_rotation * old_rots.rotation_to_correct).apply(target - source)
                 + source,
@@ -391,6 +397,15 @@ class MorphologyBender:
         old_rots.rotation_to_correct = new_to_correct
         return scaled_diff_rotation
 
+    def local_layer_thickness(self, point, return_lay=False):
+        curr_ann = self.voxel_data_of(point, self.annotations)
+        thick, lay = self._ann_to_abv(curr_ann)
+        if lay is not None:
+            thick = np.sum(self.voxel_data_of(point, self.thicknesses())[thick : thick + 2])
+        if return_lay:
+            return thick, lay
+        return thick
+
     def process_scaling(self, point):
         """
         Calculate the local scaling factor to apply to a morphology segment based on the local
@@ -400,14 +415,9 @@ class MorphologyBender:
         :return: scaling factor at the current location
         :rtype: float
         """
-        curr_ann = self.voxel_data_of(point, self.annotations)
-        thick, lay = self._ann_to_abv(curr_ann)
+        thick, lay = self.local_layer_thickness(point, return_lay=True)
         if lay is not None:
-            return np.maximum(
-                np.sum(self.voxel_data_of(point, self.thicknesses())[thick : thick + 2])
-                / self.default_depth[lay],
-                0.1,
-            )
+            return np.maximum(thick / self.default_depth[lay], 0.1)
         else:  # out of the annotations / depth / orientations fields.
             return 0.1
 
@@ -434,7 +444,7 @@ class MorphologyBender:
         if 0 <= fixed_dimension <= 2:
             new_coord[fixed_dimension] = old_coord[fixed_dimension]
         # check that every voxel between the points remain within the region boundary
-        rescale = self.is_target_wrong(branch.points[i - 1], new_coord, branch_labels)
+        rescale = self.is_scaling_wrong(branch.points[i - 1], new_coord, branch_labels)
         # if scaling resulted in an overshoot, set to old point coordinate
         branch.points[i] = np.copy(new_coord) if not rescale else np.copy(branch.points[i - 1])
         # translate all the points of the branch starting at i